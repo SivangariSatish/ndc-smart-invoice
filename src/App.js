1ed6da8ae41df0e68f98a930f696e36f1321a5fc
SivangariSatish <sivangari@gmail.com>
2023-01-19 14:57:09 +0530

    Updates

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import SmartInvoiceDetails from './components/SmartInvoiceDetails';
+
+function App() {
+  return (
+    <div className="App">
+           
+        <p>
+         <SmartInvoiceDetails>Test</SmartInvoiceDetails>
+        </p>
+        
+     
+    </div>
+  );
+}
+
+export default App;
